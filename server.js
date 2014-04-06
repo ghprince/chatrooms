c01e00b7ba2aec383b919d20543688d66cbae8b2
Gordon Gao <ghprince@gmail.com>
2014-04-06 18:23:31 +0800

    change port number

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ var server = http.createServer(function(request, response) {
   serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000.");
+server.listen(80, function() {
+  console.log("Server listening on port 80.");
 });
 
 var chatServer = require('./lib/chat_server');
