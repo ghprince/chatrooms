2497dfe738794e9e51ac088f7547f814916b2c70
Gordon Gao <ghprince@gmail.com>
2014-04-06 18:27:46 +0800

    change port to a dynamic one

--- server.js
+++ server.js
@@ -50,8 +50,9 @@ var server = http.createServer(function(request, response) {
   serveStatic(response, cache, absPath);
 });
 
-server.listen(80, function() {
-  console.log("Server listening on port 80.");
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+  console.log("Server listening on port " + port + ".");
 });
 
 var chatServer = require('./lib/chat_server');
